26f621b29189f41ebf85f970953e198135889623
GabrVendrame <gabrielvendrame7@gmail.com>
2025-01-26 13:36:45 -0300

    feat: build basic website structure

--- src/app/components/app-component/app.component.ts
+++ src/app/components/app-component/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
+import { DisclaimerComponent } from "../disclaimer/disclaimer.component";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, DisclaimerComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
