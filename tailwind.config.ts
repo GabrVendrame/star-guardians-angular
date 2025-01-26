b4916c7ed964b690bad184282722599d03b1821a
GabrVendrame <gabrielvendrame7@gmail.com>
2025-01-26 13:33:46 -0300

    feat: change font family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./src/**/*.{html, ts}"],
+  content: ["./src/**/*.{html,ts}", "./src/app/components/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat"],
+        serif: ["Montserrat"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
