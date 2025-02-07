ab203613142944882ec8dd05f914ec766333de2d
GabrVendrame <gabrielvendrame7@gmail.com>
2025-02-07 15:56:20 -0300

    feat: add routes for all pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,4 +6,19 @@ export const routes: Routes = [
     path: "",
     component: HomeComponent,
   },
+  {
+    path: "stars",
+    loadChildren: () =>
+      import("@modules/stars/stars.module").then((m) => m.StarsModule),
+  },
+  {
+    path: "enemy-stars",
+    loadChildren: () =>
+      import("@modules/enemy-stars/enemy-stars.module").then((m) => m.EnemyStarsModule),
+  },
+  {
+    path: "wallpapers",
+    loadChildren: () =>
+      import("@modules/wallpapers/wallpapers.module").then((m) => m.WallpapersModule),
+  }
 ];
--- src/app/modules/stars/stars-routing.module.ts
+++ src/app/modules/stars/stars-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AkaliComponent } from "@components/stars/akali/akali.component";
+import { KaisaComponent } from "@components/stars/kaisa/kaisa.component";
+import { OriannaComponent } from "@components/stars/orianna/orianna.component";
+import { RakanComponent } from "@components/stars/rakan/rakan.component";
+import { SennaComponent } from "@components/stars/senna/senna.component";
+import { SeraphineComponent } from "@components/stars/seraphine/seraphine.component";
+import { XayahComponent } from "@components/stars/xayah/xayah.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "akali",
+    component: AkaliComponent,
+  },
+  {
+    path: "kaisa",
+    component: KaisaComponent,
+  },
+  {
+    path: "orianna",
+    component: OriannaComponent,
+  },
+  {
+    path: "rakan",
+    component: RakanComponent,
+  },
+  {
+    path: "senna",
+    component: SennaComponent,
+  },
+  {
+    path: "seraphine",
+    component: SeraphineComponent,
+  },
+  {
+    path: "Xayah",
+    component: XayahComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
